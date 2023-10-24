c6de7dc77f20029394dc2bb5a3a71dbf6dc1fd6b
hilsonV <hilsonvilliam@gmail.com>
2023-10-24 21:05:19 +0300

    Initial Commit

---
+++ FortuneRabbit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FortuneRabbit
+//
+//  Created by apple on 24.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
